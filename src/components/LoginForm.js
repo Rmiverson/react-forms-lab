e4ef9fa69a91633c9fd25999bea9b998d646185d
Rmiverson <Rmiverson99@gmail.com>
2021-02-04 11:02:53 -0800

    done, test for TwitterMessage doesnt pass but all deliverables met

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,22 +4,50 @@ class LoginForm extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      username: "",
+      password: ""
+    };
+  }
+
+  handleUsernameChange = event => {
+    this.setState({
+      username: event.target.value
+    })
+  }
+
+  handlePasswordChange = event => {
+    this.setState({
+      password: event.target.value
+    })
+  }
+
+  handleSubmit = event => {
+    event.preventDefault()
+    let login = {
+      username: this.state.username,
+      password: this.state.password
+    }
+
+    if (!!login.username && !!login.password) {
+      this.props.handleLogin(login)
+    }
+    
   }
 
   render() {
     return (
-      <form>
+      <form onSubmit={event => this.handleSubmit(event)}>
         <div>
           <label>
             Username
-            <input id="username" name="username" type="text" />
+            <input id="username" name="username" type="text" onChange={event => this.handleUsernameChange(event) }/>
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="password" name="password" type="password" />
+            <input id="password" name="password" type="password" onChange={event => this.handlePasswordChange(event)}/>
           </label>
         </div>
         <div>
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,14 +4,35 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      counter: 0
+    };
+  }
+
+  handleChar = event => {
+    let inputLength = event.target.value.length
+    this.setState({
+      counter: inputLength
+    })
+  }
+
+  displayChar = () => {
+    let counter = this.state.counter
+    let maxChars = this.props.maxChars
+    if (counter > maxChars) {
+      return "You reached your max characters: " + counter
+    } else {
+      return counter
+    }
   }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input type="text" name="message" id="message" onChange={event => this.handleChar(event)}/>
+        <br />
+        {this.displayChar()}
       </div>
     );
   }
